a2d583c5f6ff27fe03f7ddcda6625c451855da64
Nigonbol123@gmail.com <nigonbol123@gmail.com>
2022-02-22 14:47:24 -0400

    Add redux state config || start ADD_BOOK action

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+
 import './index.css';
 import App from './components/App';
 
+import store from './redux/configureStore';
+
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root'),
 );
 
